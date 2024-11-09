07327046038f8cad253eeb7cd12ad93427020813
GuangChen2333 <guangchenworks@outlook.com>
2024-11-09 20:07:14 +0800

    feat(client): Normally print exit msg on `KeyboardInterrupt`

--- app/_client.py
+++ app/_client.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 
+import loguru
 import questionary
 import httpx
 from loguru import logger
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from app import Client
+import loguru
 
 if __name__ == '__main__':
-    client = Client(wait_time=0.3)
+    try:
+        client = Client(wait_time=0.3)
+        while True:
+            client.start()
 
-    client.start()
+    except KeyboardInterrupt:
+        loguru.logger.info("Exit!")
+        exit(0)
